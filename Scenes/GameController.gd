8e7beea87d71fec55d2634dd3e68e937390c61ad
Vincent <vincemessina@gmail.com>
2021-04-25 02:25:33 -0700

    updated things iunno

--- Scenes/GameController.gd
+++ Scenes/GameController.gd
@@ -1,23 +1,59 @@
 extends Node
 
-var score = 0
+var focus = 50
 const thought = preload("res://Scenes/Thoughts.tscn")
 var orgin = Vector2(360,408)
 var reset = false
 onready var timer = get_node("Timer")
-
+onready var label = get_node("RichTextLabel")
+onready var face = get_node("Head/Head")
+onready var bg = get_node("BG")
 func _ready():
 	randomize()
+	SilentWolf.configure({
+		  "api_key": "Lxi8p8nrJD84i08qavmCQ4tXEmGwgM4W8WQ9QcAw",
+		  "game_id": "LudemDare48",
+		  "game_version": "0.0.0",
+		  "log_level": 1
+		})
+
+	SilentWolf.configure_scores({
+		  "open_scene_on_close": "res://Scenes/Main.tscn"
+		})
+
 
 func _physics_process(delta):
+	focus += 2*delta
+	label.set_text(str(focus))
 	if(reset == false):
 		reset == true
 	elif(reset == true):
 		pass
+	if(focus > 90):
+		bg.set_frame(4) 
+	if(focus > 80 and focus < 90):
+		bg.set_frame(3) 
+	if(focus > 70 and focus < 80):
+		bg.set_frame(2) 
+	if(focus > 60 and focus < 70):
+		bg.set_frame(1) 
+	if(focus < 60):
+		bg.set_frame(0)
+	if(focus > 50):
+		face.set_frame(4)
+	if(focus < 50 and focus > 40):
+		face.set_frame(3)
+	if(focus < 40 and focus > 30):
+		face.set_frame(2)
+	if(focus < 30 and focus > 20):
+		face.set_frame(1)
+	if(focus < 20 and focus > 10):
+		face.set_frame(0)
 
 func spawnThought():
 	var screen_size = get_viewport().get_visible_rect().size
 	var new_thought = thought.instance()
+	new_thought.get_node("Sprite").set_frame(rand_range(0,5))
 	var side = randi()%3+1
 	var pos = Vector2()
 	if(side == 1):
--- Scenes/Thoughts.gd
+++ Scenes/Thoughts.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var orgin = Vector2(360,408)
 var speed = 400
+onready var world = get_tree().root.get_child(1)
+
 func _ready():
 	pass 
 
@@ -11,6 +13,10 @@ func _physics_process(delta):
 
 
 func _on_Thoughts_body_entered(body):
+	if(body.name == "Head"):
+		world.focus -= 3
+	if(body.name == "paddle"):
+		pass
 	queue_free()
 	print(body.name)
 	pass # Replace with function body.
